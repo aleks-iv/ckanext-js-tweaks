956b6a2d38122fced6d0981746db788942cfbf8a
calexandr <cherniavskyi.alexandr@linkdigital.com.au>
2021-07-16 17:07:11 +0300

    add styles

--- ckanext/js_tweaks/plugin.py
+++ ckanext/js_tweaks/plugin.py
@@ -1,9 +1,11 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
+from .helpers import get_helpers
 
 class JsTweaksPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.ITemplateHelpers)
 
     # IConfigurer
 
@@ -11,3 +13,7 @@ class JsTweaksPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('assets', 'js_tweaks')
+
+    # ITemplateHelpers
+    def get_helpers(self):
+        return get_helpers()
