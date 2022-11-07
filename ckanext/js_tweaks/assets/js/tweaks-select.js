9e885fdc31466aafcfd55e628b2ff1977336bd13
Yan Rudenko <yan.rudenko@linkdigital.com.au>
2022-11-07 20:38:56 +0200

    Update accessability

--- ckanext/js_tweaks/assets/js/tweaks-select.js
+++ ckanext/js_tweaks/assets/js/tweaks-select.js
@@ -19,8 +19,8 @@ ckan.module("tweaks-select", function ($, _) {
                         "data-index": idx,
                         "data-value": el.value,
                         "tabindex": '-1',
-                        "role": "button",
                         "class": select[0].selectedIndex === idx ? "active" : "",
+                        "aria-label": el.textContent,
                         on: {
                             click: function () {
                                 select[0].selectedIndex = idx;
@@ -33,6 +33,22 @@ ckan.module("tweaks-select", function ($, _) {
                                 var key = e.which;
                                 if(key == 13) {
                                     $(this).click();
+                                }
+                            },
+                            keydown: function(e) {
+                                let parent = $(this).parent();
+                                let children = parent.children();
+                                let next = (children.length - 1) == idx ? 0 : idx + 1;
+                                let pre = idx - 1;
+                                let key = e.which;
+
+                                if(key == 38) {
+                                    children.eq(pre).focus();
+                                    e.preventDefault();
+                                }
+                                if(key == 40) {
+                                    children.eq(next).focus();
+                                    e.preventDefault();
                                 }
                             }
                         },
