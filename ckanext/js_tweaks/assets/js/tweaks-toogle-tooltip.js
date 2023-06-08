0cc1167badabd1ae8d2e8ad17162e52218feb06b
oleksandrivaniuk <oleksandr.ivaniuk@linkdigital.com.au>
2023-06-08 14:19:58 +0300

    add changes for tooltips support with bs-5

--- ckanext/js_tweaks/assets/js/tweaks-toogle-tooltip.js
+++ ckanext/js_tweaks/assets/js/tweaks-toogle-tooltip.js
@@ -1,4 +1,11 @@
 ckan.module('tweaks-toogle-tooltip', function ($, _) {
     'use strict';
-    $('[data-toggle="tooltip"]').tooltip()
+    if (typeof bootstrap === "undefined") {
+        $('[data-toggle="tooltip"]').tooltip();
+    } else {
+        // bye-bye, jQuery
+        document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(
+            el => new bootstrap.Tooltip(el)
+        );
+    }
 });
